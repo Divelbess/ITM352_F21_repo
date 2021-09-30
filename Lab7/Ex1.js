74046442001044a997d37909ac4a1b185611785f
Brandon Divelbess <divelbes@hawaii.edu>
2021-09-30 03:48:44 -1000

    Create Ex1.js

---
+++ Lab7/Ex1.js
@@ -0,0 +1,11 @@
+var age =  21;
+
+var fav_num = 4;
+
+var day_of_birth = 6;
+
+var month_of_birth = 9;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log((age + fav_num) / day_of_birth * month_of_birth);
