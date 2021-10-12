e3cb5efe7f4e1d0e2cf6aae3e6fd2a625864f409
Brandon Divelbess <divelbes@hawaii.edu>
2021-10-11 17:28:27 -1000

    Update Ex1.js

--- Lab8/Ex1.js
+++ Lab8/Ex1.js
@@ -1,7 +1,7 @@
 require("./products_data.js");  
 var num_products = 5;
 var count = 1;
-    while(count <= num_products) {
+    while(count <= num_products/2) {
         console.log(`${count}. ${eval('name'+ count)}`);
         count++;
     }
