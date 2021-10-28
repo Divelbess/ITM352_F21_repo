2a63974c604f4ff919a990542b66f28ac0fd38ba
Brandon Divelbess <divelbes@hawaii.edu>
2021-10-28 08:25:26 -1000

    Update EX4.js

---
+++ Lab11/EX4.js
@@ -0,0 +1,18 @@
+
+var attributes = "Brandon;21;21.5;-20";
+
+var parts =attributes.split(";");
+// for(part of parts) {
+//     console.log(`${part} isNonNegInt: ${isNonNegInt(part)}`)
+// }
+
+parts.forEach((item, index) => {console.log(`part ${index} is ${(isNonNegInt(item)?'a':'not a')} quantity`);});
+function isNonNegInt(q, returnErrors = false) {
+    //Check if a string is q is a non-neg integar. If returnErrorsis true, the array of errors is returned
+    //other returns true if  q is a non-neg int.
+    errors = []; // assume no errors at first
+if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+    return returnErrors ? errors : (errors.length == 0)
+}    
